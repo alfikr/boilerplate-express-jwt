6c8700c64bab4b2c79019e3a4ed5a9c64ba19b5b
Danang <daffodil1411@gmail.com>
2021-11-22 07:42:17 +0700

    initialize app

--- src/main/index.js
+++ src/main/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = require('./app')
+const config = require('./config/config')
+const logger = require('./config/logger')
+const mongoose = require('mongoose') 
+let server;
+mongoose.connect(config.mongoose.url,config.mongoose.options).then(()=>{
+    logger.info('connected to MongoDB')
+    server = app.listen(config.port,()=>{
+        logger.info('Listening to port ${config.port}')
+    })
+})
